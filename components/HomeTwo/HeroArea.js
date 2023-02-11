8522dc28376985816b7566ba8183ff99b3ac6369
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2023-02-12 02:56:43 +0600

    static deploy to firebase and adding modified images done

--- components/HomeTwo/HeroArea.js
+++ components/HomeTwo/HeroArea.js
@@ -4,7 +4,7 @@ const HeroArea = () => {
    return (
       <>
          <section className="slider__area slider__height-2 include-bg d-flex align-items-center"
-            style={{ background: `url(assets/img/slider/2/slider-2-bg.jpg)`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
+            style={{ background: `url(assets/img/slider/2/slider-2-bg.png)`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
             <div className="container">
                <div className="row align-items-center">
                   <div className="col-xxl-6 col-lg-6">
@@ -23,8 +23,8 @@ const HeroArea = () => {
                            <img className="slider__shape-1" src="assets/img/slider/2/shape/slider-cap-1.png" alt="" />
                            <img className="slider__shape-2" src="assets/img/slider/2/shape/slider-cap-2.png" alt="" />
                            <img className="slider__shape-3" src="assets/img/slider/2/shape/slider-cap-3.png" alt="" />
-                           <img className="slider__shape-4" src="assets/img/slider/2/shape/slider-shape-1.jpg" alt="" />
-                           <img className="slider__shape-5" src="assets/img/slider/2/shape/slider-shape-2.jpg" alt="" />
+                           <img className="slider__shape-4" src="assets/img/slider/2/shape/slider-shape-1.png" alt="" />
+                           <img className="slider__shape-5" src="assets/img/slider/2/shape/slider-shape-2.png" alt="" />
                         </div>
                         <span className="slider__thumb-mask">
                            <img src="assets/img/slider/2/slider-thumb.png" alt="" />
